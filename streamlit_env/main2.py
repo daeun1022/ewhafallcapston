b1a3f4d1edbb8fcf917a878591ae3d23ae388bde
DESKTOP-1C1JT1K\Daeun <nde2171011@ewhain.net>
2024-11-29 10:41:16 +0900

    사이트 이름 변경

--- streamlit_env/main2.py
+++ streamlit_env/main2.py
@@ -19,7 +19,7 @@ openai.api_key = OPENAI_API_KEY
 
 # configuring streamlit page settings
 st.set_page_config(
-    page_title="GPT-4o Chat",
+    page_title="Chatbot Site",
     layout="centered"
 )
 
